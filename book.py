057a858d5ff313136f7a7bec4d2293e38bf8b4e4
Joan <91699152+joanAcero@users.noreply.github.com>
2023-11-25 11:44:48 +0100

    Book class generate_book_content(API not free to use?)
    Created the function generate_book_content which using the API provided generates text given a book title.
    Still have to find how to access it, seems that to use the API its necessary to pay for querys.

--- book.py
+++ book.py
@@ -1,5 +1,8 @@
-class Book:
+import requests
 
+class Book:
+    
+    API_KEY = 'bda94d92-e99f-498f-aa15-8a10e257a44b'
     current_id = 0 
     all_books = {}
 
@@ -13,13 +16,26 @@ class Book:
         self.id = Book.current_id
         Book.current_id += 1
 
+        self.text = Book.generate_book_content(title)
+
         Book.all_books[self.id] = self
 
     def __str__(self):
-        return f"Title: {self.title}, Review: {self.review}, Price: {self.price}, Image URL: {self.imageUrl}, ID: {self.id}"
+        return f"Title: {self.title}, Review: {self.review}, Price: {self.price}, Image URL: {self.imageUrl}, ID: {self.id}, Text: {self.text}"
 
     def get_all_books():
         return Book.all_books.values()
+    
+
+    def generate_book_content(self):
+
+        query = ('The text of' + str(self.title) + 'is:')
+
+        r = requests.post("https://api.deepai.org/api/text-generator",data={'text': query},headers={'api-key': Book.API_KEY})
+
+        return r.json()
+
+
 
 
     
